c02635e61568dbc7dc3e1037af5b058709163334
vijayxd <vijayxd@gmail.com>
2020-04-01 19:16:52 +0530

    commit

---
+++ precache-manifest.ffa3419118bc82d3a1273c0178ec0076.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17c863ed8c0522c03913184c7b5d10c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "954a10a426753618e119",
+    "url": "/static/css/main.0a3d9d63.chunk.css"
+  },
+  {
+    "revision": "5ff90fbcb95594bad09b",
+    "url": "/static/js/2.8fcf30f8.chunk.js"
+  },
+  {
+    "revision": "203136b256d73b86d9b5f7bd77fb7e0d",
+    "url": "/static/js/2.8fcf30f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "954a10a426753618e119",
+    "url": "/static/js/main.2975b30e.chunk.js"
+  },
+  {
+    "revision": "b363efbd26dfdee5c720",
+    "url": "/static/js/runtime-main.99a6ae5c.js"
+  }
+]);
